dc9b433e4f596c68c40a097258e5a01f773ba737
Piero Sabino <piero.sa@icloud.com>
2021-10-09 16:09:56 +0200

    Update project

--- src/App.js
+++ src/App.js
@@ -78,8 +78,9 @@ function App() {
 
 const handleLocationClick = (city) => {
   console.log(city)
-  getCityWeather();
-  getCityForecast();
+  getCityWeather(city);
+  getCityForecast(city);
+  onInputChange(city);
   setIsFavouritesSelected(false);
 }
 
@@ -120,7 +121,7 @@ const handleLocationClick = (city) => {
                     {
                       favInLocal ? <ul>
                       <CloseButton aria-label="Hide" style={{margin:'2px', borderRadius:'8px'}} onClick={() => setIsFavouritesSelected (false)}/>
-                        {favInLocal.map((fav, i) => <li key={i} style={{cursor:'pointer', fontSize:20}} onClick={(fav) => handleLocationClick(fav.location)}>{fav.location}</li>)}
+                        {favInLocal.map((fav, i) => <li key={i} style={{cursor:'pointer', fontSize:'30px'}} onClick={() => handleLocationClick(fav.location)}>{fav.location}</li>)}
                         
                       </ul> : <span>no fav data</span>
                     }
